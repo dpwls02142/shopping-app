5bae124162e22b31073e34be8a84180459348ad7
dpwls <dpwls02142@gmail.com>
2025-07-10 16:40:41 +0900

    chore: 디렉토리 구조 (#2)

---
+++ app/(deal)/page.tsx
@@ -0,0 +1,7 @@
+export default function Deal() {
+  return (
+    <div>
+      <h1>특가 페이지</h1>
+    </div>
+  );
+}
---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Product() {
+  return (
+    <div>
+      <h1>상품 페이지</h1>
+    </div>
+  );
+}
