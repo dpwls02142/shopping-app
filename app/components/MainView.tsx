f9cf5af69e0d450abde89ee8cacfff31e09fea90
dpwls <dpwls02142@gmail.com>
2025-07-11 16:50:32 +0900

    feat: main page
    - main page도 layout과 동일하게 page에서 mainView component import

---
+++ app/components/MainView.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useAppNavigation } from "@/app/hooks/useAppNavigation";
+import SwipeContainer from "./SwipeContainer";
+
+interface MainViewProps {
+  shoppingHome: React.ReactNode;
+  deal: React.ReactNode;
+}
+
+export default function MainView({ shoppingHome, deal }: MainViewProps) {
+  const { currentPage } = useAppNavigation();
+
+  return (
+    <SwipeContainer>
+      {currentPage === "home" && shoppingHome}
+      {currentPage === "deal" && deal}
+    </SwipeContainer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import PersonalizedProductList from "@/app/components/PersonalizedProductList";
+import DailyProducts from "@/app/(deal)/components/DailyProducts";
+import MainView from "@/app/components/MainView";
 
 export default function Home() {
-  return <PersonalizedProductList />;
+  return (
+    <MainView
+      shoppingHome={<PersonalizedProductList />}
+      deal={<DailyProducts />}
+    />
+  );
 }
