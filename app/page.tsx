d199b2b50f5e9528a0091e515fb6926116495640
dpwls <dpwls02142@gmail.com>
2025-07-12 18:27:36 +0900

    feat: deal navbar 포함

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import DealView from "@/app/(deal)/components/DealView";
 import DailyProducts from "@/app/(deal)/components/DailyProducts";
 import MainView from "@/app/components/MainView";
 import PersonalizedProduct from "@/app/components/PersonalizedProduct";
 
-export default function Home() {
+type Props = {
+  searchParams: {
+    tab?: string;
+    view?: string;
+  };
+};
+
+export default async function Home({ searchParams }: Props) {
+  const { tab, view } = await searchParams;
+  if (tab === "deal") {
+    return <DealView searchParams={{ view }} />;
+  }
   return (
     <MainView shoppingHome={<PersonalizedProduct />} deal={<DailyProducts />} />
   );
