1a13e1fb8fde7f7cc3d82801e0fffd71995f27e1
dpwls <dpwls02142@gmail.com>
2025-07-12 17:26:20 +0900

    feat: deaily product 데이터 불러오기

--- app/(deal)/components/DailyProducts.tsx
+++ app/(deal)/components/DailyProducts.tsx
@@ -1,8 +1,20 @@
-const DailyProducts = () => {
+import { fetchAllProductsWithDetails } from "@/lib/api/productsApi";
+
+const DailyProducts = async () => {
+  const products = await fetchAllProductsWithDetails();
+  const dailyDealProducts = products.filter(
+    (p) => p.discount?.discountType === "daily_deal"
+  );
+
   return (
     <div>
       <h2>하루 특가</h2>
       <div>할인 기간이 하루 남은 상품이에요</div>
+      <ul>
+        {dailyDealProducts.map((p) => (
+          <li key={p.product.id}>{p.product.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
