9457cf2b060deec9ba42f5f3b775013fbe4e1989
dpwls <dpwls02142@gmail.com>
2025-07-12 15:28:29 +0900

    feat: product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,25 @@
-export default function Product() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1>상품 페이지</h1>
-    </div>
-  );
+import { fetchProductDetail } from "@/lib/api/productApi";
+import ProductView from "@/app/product/components/ProductView";
+type ProductPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Product({ params }: ProductPageProps) {
+  const { id: productId } = params;
+  try {
+    const productDetail = await fetchProductDetail(productId);
+    return (
+      <div className="min-h-screen bg-gray-50">
+        <ProductView productDetail={productDetail} productId={productId} />
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div>상품을 불러올 수 없습니다.</div>
+      </div>
+    );
+  }
 }
