2b47e785619dbcb25b086275f96a75eda3d164ed
dpwls <dpwls02142@gmail.com>
2025-07-12 17:52:46 +0900

    feat: daily products에서 daily product card import

--- app/(deal)/components/DailyProducts.tsx
+++ app/(deal)/components/DailyProducts.tsx
@@ -1,4 +1,5 @@
 import { fetchAllProductsWithDetails } from "@/lib/api/productsApi";
+import DailyProductCard from "@/app/(deal)/components/DailyProductCard";
 
 const DailyProducts = async () => {
   const products = await fetchAllProductsWithDetails();
@@ -7,14 +8,14 @@ const DailyProducts = async () => {
   );
 
   return (
-    <div>
-      <h2>하루 특가</h2>
-      <div>할인 기간이 하루 남은 상품이에요</div>
-      <ul>
+    <div className="py-4">
+      <h2 className="text-xl font-bold px-4">하루 특가</h2>
+      <p className="text-gray-500 px-4">할인 기간이 하루 남은 상품이에요</p>
+      <div className="flex flex-col space-y-4 p-4 max-w-4xl mx-auto">
         {dailyDealProducts.map((p) => (
-          <li key={p.product.id}>{p.product.name}</li>
+          <DailyProductCard key={p.product.id} product={p} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
