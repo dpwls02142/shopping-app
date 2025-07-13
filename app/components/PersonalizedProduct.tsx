f0517d6a03196e7768dfd6b33f939d93ea6e1a93
dpwls <dpwls02142@gmail.com>
2025-07-13 09:26:49 +0900

    style: 시맨틱 태그를 위해 개별 product에 li 태그 추가

--- app/components/PersonalizedProduct.tsx
+++ app/components/PersonalizedProduct.tsx
@@ -56,20 +56,22 @@ const PersonalizedProductCard = ({
 }: {
   product: ProductPreviewInfo;
 }) => (
-  <Link href={`/product/${product.id}`}>
-    <ProductImage product={product} />
-    <div className="space-y-1">
-      <h3 className="text-sm font-medium text-gray-900 line-clamp-2">
-        {product.name}
-      </h3>
-      <div className="flex items-center space-x-2">
-        <ProductPrice product={product} />
+  <li key={product.id}>
+    <Link href={`/product/${product.id}`}>
+      <ProductImage product={product} />
+      <div className="space-y-1">
+        <h3 className="text-sm font-medium text-gray-900 line-clamp-2">
+          {product.name}
+        </h3>
+        <div className="flex items-center space-x-2">
+          <ProductPrice product={product} />
+        </div>
+        <div className="flex items-center text-xs text-gray-500 gap-1">
+          <ProductRating product={product} />
+        </div>
       </div>
-      <div className="flex items-center text-xs text-gray-500 gap-1">
-        <ProductRating product={product} />
-      </div>
-    </div>
-  </Link>
+    </Link>
+  </li>
 );
 
 const PersonalizedProductsContent = async () => {
