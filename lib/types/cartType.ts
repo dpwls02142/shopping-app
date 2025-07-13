8fb2f2800f46a3517072e0ec62f9c396cdd38164
dpwls <dpwls02142@gmail.com>
2025-07-13 11:53:33 +0900

    feat: cart type 정의
    - CartItem: cart에 담길 product item
    - CartState: cart items 개수, product total price
    - CartActions: cart page에서 필요한 actions(add, remove, update quantity, clear)

---
+++ lib/types/cartType.ts
@@ -0,0 +1,30 @@
+import { Product, ProductOption } from "./productType";
+
+export type CartItem = {
+  id: string;
+  product: Product;
+  selectedOptions: ProductOption[];
+  quantity: number;
+  totalPrice: number;
+  createdAt: string;
+};
+
+export type CartState = {
+  items: CartItem[];
+  totalItems: number;
+  totalPrice: number;
+};
+
+export type CartActions = {
+  addToCart: (
+    product: Product,
+    selectedOptions: ProductOption[],
+    quantity: number
+  ) => void;
+  removeFromCart: (itemId: string) => void;
+  updateQuantity: (itemId: string, quantity: number) => void;
+  clearCart: () => void;
+  getItemById: (itemId: string) => CartItem | undefined;
+};
+
+export type CartStore = CartState & CartActions;
