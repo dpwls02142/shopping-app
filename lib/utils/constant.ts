7884ddca5f380b24305387f6ad6c3d6818b431fb
dpwls <dpwls02142@gmail.com>
2025-07-14 15:38:52 +0900

    feat: getApiUrl 함수 수정
    - preview 환경에서도 동작해야됨

--- lib/utils/constant.ts
+++ lib/utils/constant.ts
@@ -1,9 +1,14 @@
-const serverBase = process.env.API_BASE_URL;
 export const getApiUrl = (endpoint: string) => {
   if (typeof window === "undefined") {
-    return `${serverBase}/api/${endpoint}`;
+    // 서버 사이드
+    if (process.env.VERCEL_URL) {
+      return `https://${process.env.VERCEL_URL}/api/${endpoint}`;
+    }
+    return `${process.env.API_BASE_URL || "http://localhost:3000"}/api/${endpoint}`;
   }
-  return `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/${endpoint}`;
+
+  // 클라이언트 사이드
+  return `${process.env.NEXT_PUBLIC_API_BASE_URL || window.location.origin}/api/${endpoint}`;
 };
 
 export const formatPriceToKor = (price: number) => {
