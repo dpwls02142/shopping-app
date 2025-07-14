aa8b11696b20cfa6ce7ad1f23764ad0d790a958f
dpwls <dpwls02142@gmail.com>
2025-07-14 14:02:07 +0900

    feat: cart page 구조 변경
    - isCartNull 변수를 추가하여 cart의 length를 한꺼번에 비교
    
    - 장바구니 버튼은 개별 컴포넌트로 분리

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,36 +7,31 @@ import { Button } from "@/components/ui/button";
 
 export default function CartPage() {
   const { items, totalItems } = useCartProductsStore();
-
+  const isCartNull = items.length === 0;
   return (
-    <div className="absolute inset-0 flex flex-col">
-      <div className="flex-1">
-        <div className="mx-auto px-4 py-14">
-          <div className="mb-4">
-            <h1 className="text-xl font-bold text-gray-900">장바구니</h1>
+    <div className="flex flex-col min-h-full relative">
+      <div className="px-4 pt-14 pb-60">
+        <h1 className="text-xl font-bold text-gray-900 mb-4">장바구니</h1>
+        {isCartNull ? (
+          <div className="flex flex-col items-center justify-center h-full">
+            <div className="text-gray-500 text-xl mb-4">
+              장바구니가 비어있습니다
+            </div>
           </div>
-
-          {items.length === 0 ? (
-            <div className="flex flex-col items-center justify-center">
-              <div className="text-gray-500 text-xl mb-4">
-                장바구니가 비어있습니다
-              </div>
-            </div>
-          ) : (
-            <div>
-              <div className="grid grid-cols-1 gap-6">
-                <CartProducts />
-                <CartProductsSummary />
-              </div>
-              <div className="bg-white p-4 sticky bottom-0">
-                <Button className="w-full outline-none text-lg font-bold h-12">
-                  {totalItems}건 주문하기
-                </Button>
-              </div>
-            </div>
-          )}
-        </div>
+        ) : (
+          <div className="grid grid-cols-1 gap-6">
+            <CartProducts />
+            <CartProductsSummary />
+          </div>
+        )}
       </div>
+      {!isCartNull && (
+        <div className="sticky bottom-0 bg-white border-t p-4">
+          <Button className="w-full outline-none text-lg font-bold h-12">
+            {totalItems}건 주문하기
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
