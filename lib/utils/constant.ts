0d1036a507b366d60eacdfb8afd6534724d7c362
dpwls <dpwls02142@gmail.com>
2025-07-14 15:47:24 +0900

    test: vercelUrl있을 땐 vercel url로 그 외엔 동적

--- lib/utils/constant.ts
+++ lib/utils/constant.ts
@@ -1,19 +1,17 @@
 export const getApiUrl = (endpoint: string) => {
   if (typeof window === "undefined") {
-    console.log("Server side environment:");
-    console.log("VERCEL_URL:", process.env.VERCEL_URL);
-    console.log("VERCEL_ENV:", process.env.VERCEL_ENV);
-    console.log("API_BASE_URL:", process.env.API_BASE_URL);
+    const vercelUrl = process.env.VERCEL_URL;
+    const apiBaseUrl = process.env.API_BASE_URL;
 
-    // 서버 사이드
-    if (process.env.VERCEL_URL) {
-      return `https://${process.env.VERCEL_URL}/api/${endpoint}`;
+    if (vercelUrl) {
+      return `https://${vercelUrl}/api/${endpoint}`;
     }
-    return `${process.env.API_BASE_URL || "http://localhost:3000"}/api/${endpoint}`;
+    if (apiBaseUrl) {
+      return `${apiBaseUrl}/api/${endpoint}`;
+    }
+    return `http://localhost:3000/api/${endpoint}`;
   }
-
-  // 클라이언트 사이드
-  return `${process.env.NEXT_PUBLIC_API_BASE_URL || window.location.origin}/api/${endpoint}`;
+  return `/api/${endpoint}`;
 };
 
 export const formatPriceToKor = (price: number) => {
