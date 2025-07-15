17fbe5784fd16d8dc53cdf9b8631a1953eb167a6
dpwls <dpwls02142@gmail.com>
2025-07-15 19:35:51 +0900

    style: cart page 배경 회색으로 변경
    - cart product card의 배경을 흰색으로 변경하며 전체 배경은 회색으로

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -10,7 +10,7 @@ export default function CartPage() {
   const { items, totalItems } = useCartProductsStore();
   const isCartNull = items.length === 0;
   return (
-    <div className="flex flex-col min-h-full relative">
+    <div className="flex flex-col min-h-full relative bg-gray-50">
       <div className="px-4 pt-14 pb-60">
         <h1 className="text-xl font-bold text-gray-900 mb-4">장바구니</h1>
         {isCartNull ? (
