fc6bbb3285d647b9490a0dda65339c21a7e401a6
dpwls <dpwls02142@gmail.com>
2025-07-16 19:53:17 +0900

    refactor: product overview에서 flex 관련은 상수 사용

--- src/app/product/[id]/components/ProductOverview.tsx
+++ src/app/product/[id]/components/ProductOverview.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { toProductPreview } from "@/lib/utils/productOptionUtils";
 import { ProductDetailInfo } from "@/lib/types/productType";
 
@@ -7,6 +8,7 @@ import DealTimer from "@/app/product/(deal)/components/DealTimer";
 import ProductImage from "@/app/product/components/ProductImage";
 import ProductPrice from "@/app/product/components/ProductPrice";
 import ProductRating from "@/app/product/components/ProductRating";
+import { FLEX_ITEMS_START_BETWEEN } from "@/lib/styles";
 
 const ProductOverview = ({
   productDetail,
@@ -23,7 +25,8 @@ const ProductOverview = ({
         className="w-full aspect-square bg-gray-100"
       />
       <div className="p-4">
-        <div className="flex justify-between items-start mb-4">
+
+        <div className={cn(FLEX_ITEMS_START_BETWEEN, "mb-4")}>
           <h1 className="text-xl font-bold text-gray-900 flex-1">
             {product.name}
           </h1>
@@ -33,12 +36,14 @@ const ProductOverview = ({
             size="large"
           />
         </div>
+
         <ProductPrice
           product={toProductPreview(productDetail)}
           size="large"
           showDiscountRate={true}
         />
         {discount?.discountType === "daily_deal" && <DealTimer />}
+        
       </div>
     </div>
   );
