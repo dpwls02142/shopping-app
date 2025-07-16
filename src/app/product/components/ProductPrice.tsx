9f3e7a7093b4503f161515a115c5f868edc8f771
dpwls <dpwls02142@gmail.com>
2025-07-16 20:03:56 +0900

    refactor: product price 상태 하나로 통합

--- src/app/product/components/ProductPrice.tsx
+++ src/app/product/components/ProductPrice.tsx
@@ -6,14 +6,9 @@ import { PRODUCT_PRICE_SIZE } from "@/lib/styles";
 type ProductPriceProps = {
   product: ProductPreviewInfo;
   size?: "small" | "medium" | "large";
-  showDiscountRate?: boolean;
 };
 
-function ProductPrice({
-  product,
-  size = "medium",
-  showDiscountRate = false,
-}: ProductPriceProps) {
+function ProductPrice({ product, size = "medium" }: ProductPriceProps) {
   const { basePrice, discountedPrice, discountRate } = product;
 
   const classes = PRODUCT_PRICE_SIZE[size];
@@ -24,19 +19,19 @@ function ProductPrice({
         <span className={classes.main}>{formatPriceToKor(basePrice)}원</span>
       </div>
     );
-  }
-
-  return (
-    <div>
-      {showDiscountRate && discountRate && (
+  } else {
+    return (
+      <div className="space-x-2">
         <span className={classes.discount}>{discountRate}%</span>
-      )}
-      <span className={classes.main}>
-        {formatPriceToKor(discountedPrice)}원
-      </span>
-      <span className={classes.secondary}>{formatPriceToKor(basePrice)}원</span>
-    </div>
-  );
+        <span className={classes.main}>
+          {formatPriceToKor(discountedPrice)}원
+        </span>
+        <span className={classes.secondary}>
+          {formatPriceToKor(basePrice)}원
+        </span>
+      </div>
+    );
+  }
 }
 
 export default ProductPrice;
