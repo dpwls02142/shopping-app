120cf1bb9a0d5796b33fc1aafbd22c71304cb0ab
dpwls <dpwls02142@gmail.com>
2025-07-17 10:43:17 +0900

    refactor: navigation에서 불필요한 type 제거

--- src/lib/types/navgationType.ts
+++ src/lib/types/navgationType.ts
@@ -5,14 +5,12 @@ type AppNavigationState = {
   previousPage: NavigationPage | null;
   showHeader: boolean;
   showNavbar: boolean;
-  showBackButton: boolean;
 };
 
 type AppNavigationActions = {
   navigateTo: (page: NavigationPage) => void;
   setShowHeader: (show: boolean) => void;
   setShowNavbar: (show: boolean) => void;
-  setShowBackButton: (show: boolean) => void;
 };
 
 type NavigationStore = AppNavigationState & AppNavigationActions;
