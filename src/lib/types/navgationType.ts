28ad818f95d1898b61e13048049b35b50d684d9f
dpwls <dpwls02142@gmail.com>
2025-07-17 11:22:18 +0900

    chore: navigaiton type 제거

--- src/lib/types/navgationType.ts
+++ src/lib/types/navgationType.ts
@@ -1,21 +1,3 @@
 type NavigationPage = "home" | "product" | "cart" | "deal";
 
-type AppNavigationState = {
-  currentPage: NavigationPage;
-  previousPage: NavigationPage | null;
-  showNavbar: boolean;
-};
-
-type AppNavigationActions = {
-  navigateTo: (page: NavigationPage) => void;
-  setShowNavbar: (show: boolean) => void;
-};
-
-type NavigationStore = AppNavigationState & AppNavigationActions;
-
-export type {
-  AppNavigationActions,
-  AppNavigationState,
-  NavigationPage,
-  NavigationStore,
-};
+export type { NavigationPage };
