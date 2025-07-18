5f82521aa0dfcd8879b139abede6d5a0ea9b9a42
dpwls <dpwls02142@gmail.com>
2025-07-18 12:45:51 +0900

    feat: cart page가 아닐 때 point display 등장

--- src/app/_shared/components/ConditionalLayout.tsx
+++ src/app/_shared/components/ConditionalLayout.tsx
@@ -1,25 +1,33 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 
 import { MAIN_CONTAINER } from "@/lib/styles";
-
 import { useAppNavigation } from "@/app/_shared/hooks/useAppNavigation";
-
 import AppHeader from "@/app/_shared/components/AppHeader";
 import AppNavbar from "@/app/_shared/components/AppNavbar";
+import PointDisplay from "@/app/_point/components/PointDisplay";
+import { useScrollActivity } from "@/app/_point/hooks/useScrollActivity";
 
 interface ConditionalLayoutProps {
   children: ReactNode;
 }
 
 function ConditionalLayout({ children }: ConditionalLayoutProps) {
-  const { isMainPage } = useAppNavigation();
+  const { isMainPage, currentPage } = useAppNavigation();
+  const mainRef = useRef<HTMLElement>(null);
+  const isCartPage = currentPage === "cart";
+
+  useScrollActivity(mainRef, !isCartPage);
+
   return (
     <div className="h-full flex flex-col">
       <AppHeader />
       {isMainPage && <AppNavbar />}
-      <main className={MAIN_CONTAINER}>{children}</main>
+      {!isCartPage && <PointDisplay />}
+      <main ref={mainRef} className={MAIN_CONTAINER}>
+        {children}
+      </main>
     </div>
   );
 }
