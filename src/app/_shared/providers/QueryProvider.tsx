8b15d452d1c02b1a430b8db3f83b83eb3bd8ee8d
dpwls <dpwls02142@gmail.com>
2025-07-26 09:11:10 +0900

    feat: react query에서 mutations error 처리

--- src/app/_shared/providers/QueryProvider.tsx
+++ src/app/_shared/providers/QueryProvider.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { notification } from "@/lib/utils/notification";
 
 function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -12,10 +13,14 @@ function QueryProvider({ children }: { children: React.ReactNode }) {
             staleTime: 60 * 1000,
             retry: 1,
           },
+          mutations: {
+            onError: (error: Error) => {
+              notification.error(error.message);
+            },
+          },
         },
-      }),
+      })
   );
-
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
