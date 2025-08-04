4c4e42dba024829c0f5fe744cc2c46c28a7917b8
dpwls <dpwls02142@gmail.com>
2025-08-04 23:32:20 +0900

    chore: import 순서 수정

--- src/scripts/randomProductImage.mjs
+++ src/scripts/randomProductImage.mjs
@@ -1,6 +1,6 @@
+import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
-import dotenv from 'dotenv';
 import { Readable } from 'stream';
 
 dotenv.config({ path: path.resolve(process.cwd(), '.env.local') });
