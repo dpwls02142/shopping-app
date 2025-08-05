a5f21c54e568a8810530ad098e568b5ecadfe4c2
dpwls <dpwls02142@gmail.com>
2025-08-05 19:21:39 +0900

    refactor: add to cart button 클릭 핸들러를 직접 onClick으로 변경하여 코드 간소화

--- src/_shared/modules/product/components/AddToCartButton.tsx
+++ src/_shared/modules/product/components/AddToCartButton.tsx
@@ -9,16 +9,12 @@ interface AddToCartButtonProps {
 }
 
 function AddToCartButton({ onClick }: AddToCartButtonProps) {
-  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    onClick?.(event);
-  };
-
   return (
     <Button
       variant="outline"
       size="sm"
       className="p-2 h-8 w-8 hover:bg-blue-50 hover:border-blue-200"
-      onClick={handleButtonClick}
+      onClick={onClick}
       aria-label="장바구니에 추가"
     >
       <ShoppingCart className="h-4 w-4" />
