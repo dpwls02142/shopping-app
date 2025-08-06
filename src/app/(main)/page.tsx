c2a3b6a4fa511a186bd53a3587fd4ceb54b49384
dpwls <dpwls02142@gmail.com>
2025-08-06 11:41:45 +0900

    chore: main page meta data 설정

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,18 @@
+import type { Metadata } from "next";
+
 import { AppSwipeNavbar } from "@/_shared/components/AppSwipeNavbar";
 import { PersonalizedProductList } from "@/_shared/modules/product/components/PersonalizedProductList";
 
-export default function HomePage() {
+export const metadata: Metadata = {
+  title: `쇼핑몰 홈`,
+};
+
+function HomePage() {
   return (
     <AppSwipeNavbar>
       <PersonalizedProductList />
     </AppSwipeNavbar>
   );
 }
+
+export default HomePage;
