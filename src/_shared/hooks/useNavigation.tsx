72ad9f694aa10d15ac4d581eed13d75bb5f3c02e
dpwls <dpwls02142@gmail.com>
2025-08-06 18:26:15 +0900

    refactor: use navigation에서 사용되던 page count storage는 불필요하니 제거, session key값은 상수로 관리

--- src/_shared/hooks/useNavigation.tsx
+++ src/_shared/hooks/useNavigation.tsx
@@ -2,28 +2,19 @@
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
+const ENTRY_PAGE_KEY = "entry-page";
+
 function useNavigation() {
   const [canShowBackButton, setCanShowBackButton] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
-    const entryPage = sessionStorage.getItem("entry-page");
-    const currentPageCount = parseInt(
-      sessionStorage.getItem("page-count") || "0"
-    );
-
+    const entryPage = sessionStorage.getItem(ENTRY_PAGE_KEY);
     if (!entryPage) {
-      // 첫 진입
-      sessionStorage.setItem("entry-page", pathname);
-      sessionStorage.setItem("page-count", "1");
+      sessionStorage.setItem(ENTRY_PAGE_KEY, pathname);
       setCanShowBackButton(false);
     } else {
-      // 첫 진입이 아니라면 page count 증가
-      const newPageCount = currentPageCount + 1;
-      sessionStorage.setItem("page-count", newPageCount.toString());
-
-      if (pathname === entryPage) setCanShowBackButton(false);
-      else setCanShowBackButton(newPageCount >= 2);
+      setCanShowBackButton(pathname !== entryPage);
     }
   }, [pathname]);
 
