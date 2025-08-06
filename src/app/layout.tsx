75f3f50a2d2de3c9e3ce0ff83bc341bade62b729
dpwls <dpwls02142@gmail.com>
2025-08-06 11:04:47 +0900

    chore: html lang kr -> ko로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="kr" className={`${pretendard.variable}`}>
+    <html lang="ko" className={`${pretendard.variable}`}>
       <body className={pretendard.className}>
         <QueryProvider>
           <div className="h-screen bg-gray-100 flex justify-center overflow-hidden">
