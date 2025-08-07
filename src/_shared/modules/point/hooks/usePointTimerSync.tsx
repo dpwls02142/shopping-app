8a74b3328be8463180d210387018b7744a06fc33
dpwls <dpwls02142@gmail.com>
2025-08-07 18:49:45 +0900

    feat: 탭이 보여지거나 탭에 포커싱 될 때만 storage 상태를 즉시 반영하기 위한 point timer sync hook 추가

---
+++ src/_shared/modules/point/hooks/usePointTimerSync.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePointTimerStore } from "@/_shared/modules/point/stores/usePointTimerStore";
+
+/**
+ * 탭 간 포인트 타이머 상태 동기화를 위한 훅
+ */
+function usePointTimerSync() {
+  const { syncStateFromStorage, initializeSession } = usePointTimerStore();
+
+  useEffect(() => {
+    syncStateFromStorage();
+
+    initializeSession();
+
+    const handleFocus = () => {
+      syncStateFromStorage();
+    };
+
+    const handleVisibilityChange = () => {
+      if (!document.hidden) {
+        syncStateFromStorage();
+      }
+    };
+
+    window.addEventListener("focus", handleFocus);
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, [syncStateFromStorage, initializeSession]);
+}
+
+export { usePointTimerSync };
